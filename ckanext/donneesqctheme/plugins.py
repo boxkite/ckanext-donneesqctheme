6084923f60548b27cf1d12a7b8e56b12096adf50
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-03-12 15:10:19 -0400

    resource views on dataset pages

--- ckanext/donneesqctheme/plugins.py
+++ ckanext/donneesqctheme/plugins.py
@@ -1,4 +1,6 @@
 import ckan.logic as logic
+import ckan.model as model
+import ckan.lib.dictization.model_dictize as md
 
 from ckan.plugins import (toolkit, IConfigurer, SingletonPlugin, implements,
     IRoutes, IConfigurer, ITemplateHelpers, IGroupForm, IPackageController,
@@ -101,6 +103,20 @@ class PackagePlugin(SingletonPlugin):
         related_list = logic.get_action('related_list')({}, pkg_dict)
         pkg_dict['related_list'] = related_list
 
+        res_ids = [res['id'] for res in pkg_dict['resources']]
+
+        context = {'model': model, 'session': model.Session}
+        view = model.Session.query(model.ResourceView).filter(
+            model.ResourceView.resource_id.in_(res_ids)).filter(
+            model.ResourceView.featured == True
+        ).first()
+
+        pkg_dict['view'] = md.resource_view_dictize(view, context)
+
+        if pkg_dict['view']:
+            pkg_dict['view_res'] = [res for res in pkg_dict['resources']
+                    if res['id'] == pkg_dict['view']['resource_id']][0]
+
         return pkg_dict
 
 
@@ -118,7 +134,7 @@ def _get_organizations():
 def _get_group_list():
 
     groups = logic.get_action('group_list')(
-        data_dict={'all_fields': True})    
+        data_dict={'all_fields': True})
 
     return groups
 
@@ -141,4 +157,4 @@ def _get_carousel_content():
         f = codecs.open(path ,'r', encoding='utf8')
         carousel_html += f.read()
     '''
-    return "<div>meuh</div>" 
+    return "<div>meuh</div>"
